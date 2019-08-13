6f88e82820128cf9ea0af35b0c27bd6f7a621714
Simon <shwaldemar@gmail.com>
2019-08-13 17:00:02 +0100

    found missing bracket all well in browser

--- src/components/MusicSelector.js
+++ src/components/MusicSelector.js
@@ -3,7 +3,7 @@ import React from 'react';
 const MusicSelector = (props) => {
 
   const feed = props.tunes.map((tune, index) => {
-    return <option value={index} key={index}>{tune["im:name"]}<option>
+    return <option value={index} key={index}>{tune["im:name"]}</option>
   });
 
   function handleChange(event) {
@@ -11,7 +11,10 @@ const MusicSelector = (props) => {
   }
 
   return(
-    <select id="music-selector" defaultValue="default" onChange={handleChange}
+    <select
+    id="music-selector"
+    defaultValue="default"
+    onChange={handleChange}
     >
       <option disabled value="default">Choose a Tune</option>
     </select>
